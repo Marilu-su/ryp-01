0a4aacbf5c55112b2d18422315ea3accfff4c672
Marilu-su <102176415+Marilu-su@users.noreply.github.com>
2022-03-28 14:22:24 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,19 @@
+import socket as s
+IP = s.gethostbyname(s.gethostname())
+PORT = 8000
+HEADER = 1024
+server= s.socket()
+server.bind((IP,PORT))
+server.listen()
 
+While True:
+    conn, add = server.accept()
+    connected= True
+    print (f"nueva coneccion{addr}")
+    while connected:
+        length = conn.recv(HEADER)
+        length = int(length.decode("utf-8"))
+        msg = conn.recv(legth)
+        print(f"{msg.decode(´utf-8´)}")
+    
+    

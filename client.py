005c4464cca6e644b39e894d1c563d4a80b2fd45
Marilu-su <102176415+Marilu-su@users.noreply.github.com>
2022-03-28 14:30:46 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,4 +1,14 @@
 # ryp-01
 import socket
-
-IP = socket
+IP = socket.gethostbyname(socket.gethostname())
+PORT = 8000
+HEADER= 1024
+client=socket.socket()
+client.connect((IP, PORT))
+while True:
+  msg= input("Ingrese un mensaje:")
+  length = len(msg)
+msg_length = f"´{length}"
+msg_length +="" *(HEADER-len(msg_length))
+client.send(msg_legth.encode("utf-8"))
+client.send(msg.encode("utf-8"))

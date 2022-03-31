13a99882c805f09f1764cb41c432a534d095ca1b
Marilu-su <102176415+Marilu-su@users.noreply.github.com>
2022-03-31 19:11:26 +0200

    Update client.py

--- client.py
+++ client.py
@@ -2,15 +2,12 @@
 import socket
 IP = socket.gethostbyname(socket.gethostname())
 PORT = 8000
-HEADER= 0
+HEADER= 1024
 
 client = socket.socket()
 client.connect((IP, PORT))
 print(f"[CONNECTED] Conectado al servidor en {IP}")
 while True:
   msg= input("Ingrese un mensaje:")
-  len(msg)
-  msg_length = f"´{length}"
-  msg_length +="" *(HEADER-len(msg_length))
-  client.send(msg_legth.encode("utf-8"))
   client.send(msg.encode("utf-8"))
+

1c638ce6adf3ae75035439baa0a82678bb900d2e
Marilu-su <102176415+Marilu-su@users.noreply.github.com>
2022-05-06 16:08:27 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
-# ryp-01
 import socket
+from statistics import median_grouped
 IP = socket.gethostbyname(socket.gethostname())
 PORT = 8000
 HEADER= 1024
@@ -7,7 +7,14 @@ HEADER= 1024
 client = socket.socket()
 client.connect((IP, PORT))
 print(f"[CONNECTED] Conectado al servidor en {IP}")
+
 while True:
-  msg= input("Ingrese un mensaje:")
-  client.send(msg.encode("utf-8"))
+    msg= input("Ingrese un mensaje:")
+    msgencd=msg.encode("utf-8")
+    medidareal=str(len(msgencd)).encode("utf-8")
+    size=len(medidareal)
+    cuenta= size*(HEADER-size)
+    client.send(cuenta)
+    client.send(msgencd)
+
 

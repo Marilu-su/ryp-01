6ecb6b28295a25fc46f137248071f31a60d2b1fe
Marilu-su <102176415+Marilu-su@users.noreply.github.com>
2022-05-06 16:07:01 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,21 +1,22 @@
 import socket as s
+
 IP = s.gethostbyname(s.gethostname())
 PORT = 8000
-HEADER = 0
+HEADER = 1024
+
 server= s.socket()
 server.bind((IP,PORT))
 server.listen()
 print(f"[LISTENGING] El servidor esta esperando conexiones en {IP}")
 
-While True:
-    conn, add = server.accept()
-    connected= True
-    print (f"nueva coneccion{addr}")
-    while connected:
-        msg = conn.recv()
-        print(f"{msg.decode(´utf-8´)}")
-        print(f"Recibido")
-        if (msg=="close")
-        connected= False
-    
-    
+conn, add = server.accept()
+connected= True
+print (f"nueva coneccion{add}")
+
+while connected:
+    encoded=conn.recv(HEADER)
+    msg=encoded.decode("utf-8")
+    mensaje=conn.recv(msg).decode("utf-8")
+    print(f"[CLIENT]Recibi:{mensaje}")
+    if mensaje == "close" :
+            break
